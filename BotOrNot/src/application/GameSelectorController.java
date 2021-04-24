db732014c20b05f49d149522ba8ab5a08368b8dd
Saul Carreon <sacarreon18@gmail.com>
2021-04-23 23:25:23 -0500

    adding the project folder
    all I created was the main.java, maincontroller.java, gameselector.java, main.fxml, and gameselector.fxml file. There isn't much to it but it's a start.

---
+++ BotOrNot/src/application/GameSelectorController.java
@@ -0,0 +1,49 @@
+// Saul Carreon sme577
+// Amber Jones eyq439
+// Meera Patel tar927
+
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+
+public class GameSelectorController {
+	@FXML
+	public Button fortniteButton;
+	public Button warzoneButton;
+	public Button apexButton;
+	public Button menuButton;
+	public AnchorPane mainPane;
+	
+	@FXML
+	void warzonePressed(ActionEvent event) {
+		// warzone thingy lol
+	}
+	
+	@FXML
+	void fortnitePressed(ActionEvent event) {
+		// fortnite thingy lol
+	}
+	
+	@FXML
+	void apexPressed(ActionEvent event) {
+		// apex thingy lol
+	}
+	
+	@FXML
+	void menuPressed(ActionEvent event) throws IOException {
+		mainPane = FXMLLoader.load(getClass().getResource("Main.fxml"));
+		Scene scene = new Scene(mainPane);
+		Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+		window.setScene(scene);
+		window.show();
+	}
+}
